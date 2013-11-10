9527560e4cdc97cef34e15b7b1127a69d6046d33
Alexandre Camsonne <camsonne@halla120.jlab.org>
2013-11-10 15:06:35 -0500

    Add DDVCS variables and methods

--- include/ddvcs/DDVCSEventGen.hh
+++ include/ddvcs/DDVCSEventGen.hh
@@ -1,12 +1,32 @@
 #ifndef _DDVCSEventGen_
 #define _DDVCSEventGen_
 #include "G4SBSEventGen.hh"
+#include "G4RotationMatrix.hh"
 
 class DDVCSEventGen : public G4SBSEventGen
 {
 public:
   DDVCSEventGen();
   ~DDVCSEventGen();
+  G4LorentzVector GetQP(){return fQP;}
+  G4LorentzVector GetQM(){return fQM;}
+   void SetPairE(double v){fPairE = v;}
+        void SetQprime2(double v){fQPrime2 = v;}
+        void SetPairPart(G4String v){fPairPart = v;}
+        void SetPairCAngle(double v){fPairCAngle = v;}
+        void SetPairRotAngle(double v){fPairRotAngle = v;}
+        void SetPairDThetaAngle(double v){fPairDThetaAngle = v;}
+        void SetPairPhiAngle(double v){fPairPhiAngle = v;}
+        G4String GetPairPart(){return fPairPart;}
+        double GetPairCAngle(){return fPairCAngle;}
+        double GetPairPhiAngle(){return fPairPhiAngle;}
+        double GetPairDThetaAngle(){return fPairDThetaAngle;}
+        double GetPairRotAngle(){return fPairRotAngle;}
+  void GeneratePair();
+protected:
+  double  fPairE, fPairCAngle,fPairPhiAngle,fPairRotAngle,fPairDThetaAngle,fQPrime2;
+    G4LorentzVector fQM,fQP;
+    G4String fPairPart; 
 }
   ;
 #endif

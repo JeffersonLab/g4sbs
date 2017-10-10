69fe99cb82eccfa9c3570d083deba1b155d9295d
Richard Obrecht <obrecht@phys.uconn.edu>
2017-10-10 17:29:02 -0400

    Needed to remove cmake files, my mistake

--- CMakeFiles/G4SBS.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/G4SBS"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/G4SBS.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- CMakeFiles/g4sbsroot.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for g4sbsroot.
-# This may be replaced when dependencies are built.

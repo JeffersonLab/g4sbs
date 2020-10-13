c3a7239c921bfdbad1154d2632c3da13143f06af
David Flay <flay@enpflay.jlab.org>
2020-10-12 20:13:17 -0500

    Minor tweak to BDParameterisation -- changed data member pointers to regular arrays

--- src/G4SBSBeamlineBuilder.cc
+++ src/G4SBSBeamlineBuilder.cc
@@ -5575,7 +5575,7 @@ void G4SBSBeamlineBuilder::MakeBeamExit(G4LogicalVolume *logicMother,G4double dz
    // CheckZPos(logicMother,z_bd); 
    MakeBeamExit_TargetToMidPipe(logicMother,z_tmp);  
    MakeBeamExit_MidPipeToDump(logicMother,z_mpd); 
-   if(fDetCon->GetBeamDumpEnable())MakeBeamDump(logicMother); 
+   if(fDetCon->GetBeamDumpEnable()) MakeBeamDump(logicMother); 
 }
 
 void G4SBSBeamlineBuilder::MakeBeamExit_TargetToMidPipe(G4LogicalVolume *logicMother,G4double z0){

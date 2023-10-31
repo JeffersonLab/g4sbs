53619a3754b6dbf99a01bcb322dc72727942c144
Sebastian Seeds <sseeds@ifarm1901.jlab.org>
2023-10-31 15:32:08 -0400

    Added required header files for recent changes.

--- include/G4SBSIO.hh
+++ include/G4SBSIO.hh
@@ -30,7 +30,7 @@ class G4SBSGlobalField;
 
 //These aren't really "event"-level quantities, as they are constants describing the setup, and should be stored in the "rundata" object.
 typedef struct {
-  Double_t thbb, thsbs, dbb, dsbs, dhcal,voffhcal, hoffhcal, drich, dsbstrkr, sbstrkrpitch, dlac, vofflac, hofflac, Ebeam, Ibeam;
+  Double_t thbb, thsbs, dbb, dsbs, dhcal, voffhcal, hoffhcal, angoffhcal, drich, dsbstrkr, sbstrkrpitch, dlac, vofflac, hofflac, Ebeam, Ibeam;
 } gen_t;
 
 
@@ -138,6 +138,7 @@ public:
   void SetHcalDist(double d){ gendata.dhcal = d/CLHEP::m; }
   void SetHcalVOffset(double d){ gendata.voffhcal = d/CLHEP::m; }
   void SetHcalHOffset(double d){ gendata.hoffhcal = d/CLHEP::m; }
+  void SetHcalAngOffset(double th){ gendata.angoffhcal = th; }
   void SetLACDist( double d){ gendata.dlac = d/CLHEP::m; }
   void SetLACVOffset( double d ){ gendata.vofflac = d/CLHEP::m; }
   void SetLACHOffset( double d ){ gendata.hofflac = d/CLHEP::m; }

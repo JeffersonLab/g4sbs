7c4da40beaa76aeba7ea154aee76b479c58eb17b
Andrew Puckett <puckett@jlab.org>
2024-10-02 14:47:47 -0400

    major update, speed improvements, and bug fixes for GEP elastic trigger analysis macro and coincidence lookup table generation script

--- root_macros/ECAL_HCAL_Correlations.C
+++ root_macros/ECAL_HCAL_Correlations.C
@@ -13,26 +13,30 @@ void ECAL_HCAL_Correlations(const char *rootfilename, const char *outfilename, d
   ofstream outfile(outfilename);
 
   TH2D *hcorrplot;
-  fin->GetObject("hmaxnode_ECAL_vs_HCAL",hcorrplot);
+  fin->GetObject("hallbins_ECAL_vs_HCAL",hcorrplot);
 
   TH1D *htemp;
 
   map<int,set<int> > ECAL_list_HCAL;
   
   for( int i=1; i<=hcorrplot->GetNbinsX(); i++ ){
-    int node = i;
+    int nodeH = int(hcorrplot->GetXaxis()->GetBinCenter(i));
+
+    cout << "(i, HCAL bin)=(" << i << ", " << nodeH << ")" << endl;
     htemp = hcorrplot->ProjectionY("",i,i);
 
     double total = htemp->Integral(1,htemp->GetNbinsX());
     
     for( int j=1; j<=htemp->GetNbinsX(); j++ ){
-      if( htemp->GetBinContent(j)/total >= threshold ){
-	ECAL_list_HCAL[node].insert(j);
+      if( htemp->GetBinContent(j)/total > threshold ){
+	int nodeE = int(htemp->GetBinCenter(j));
+	cout << "(j,ECAL bin)=(" << j << ", " << nodeE << ")" << endl;
+	ECAL_list_HCAL[nodeH].insert(nodeE);
       }
     }
 
-    outfile << node << " " << ECAL_list_HCAL[node].size();
-    for( set<int>::iterator k=ECAL_list_HCAL[node].begin(); k!=ECAL_list_HCAL[node].end(); ++k ){
+    outfile << nodeH << " " << ECAL_list_HCAL[nodeH].size();
+    for( set<int>::iterator k=ECAL_list_HCAL[nodeH].begin(); k!=ECAL_list_HCAL[nodeH].end(); ++k ){
       outfile << " " << *k; 
     }
     outfile << endl;
